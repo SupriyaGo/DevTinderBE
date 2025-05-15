ba28c7bf5cbe3ca48c4cfd408c1fc3c2968b47ec
Supriya Gorai <115802703+supriya-gorai-p360@users.noreply.github.com>
2025-05-15 02:19:30 -0400

    Exploreing GET, POST, DELETE method

--- src/app.js
+++ src/app.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const app = express();
 
-app.use("/",(req, res) => {
-    res.send('Hello, World!');
+// app.use("/test",(req, res) => {
+//     res.send('Hello, Test!');
+// })
+
+let data = {
+    firstname: "John",
+    lastname: "Doe",
+    age: 30,
+    city: "New York"
+};
+app.get("/user",(req, res) => {
+    res.send(data);
 })
-app.use("/hello",(req, res) => {
-    res.send('Hello, Hello!');
-})
-app.use("/test",(req, res) => {
-    res.send('Hello, Test!');
-})
+
+app.post("/user",(req, res) => {
+   // Saving data to DB
+    console.log("Saving data to DB");
+    
+    res.send("Successfully saved data to DB");
+});
+
+app.delete("/user",(req, res) => {
+   // Saving data to DB
+    console.log("Deleting data from DB");
+    
+    res.send("Successfully deleted data from DB");
+});
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
